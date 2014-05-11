1dce5e920acce58422b9eb57b68322e0cc2dad79
Rajat Upadhyaya <rajat@urbanladder.com>
2014-05-11 07:16:04 +0000

    Enable creating sale order with test/fixtures

--- lib/uniware/client.rb
+++ lib/uniware/client.rb
@@ -1,9 +1,20 @@
+require 'savon'
+
 module Uniware
   class Client
     extend Savon::Model
 
-    BASE_URL "https://%s/services/soap/?version=1.5"
     namespace "http://uniware.unicommerce.com/services/"
+    BASE_URL = "https://%s/services/soap/?version=1.5"
+    SALE_ORDER_XML = <<-SXML
+      <ser:SaleOrder>
+        %s
+        <ser:Addresses>%s</ser:Addresses>
+        %s
+        <ser:SaleOrderItems>%s</ser:SaleOrderItems>
+        <ser:CustomFields>%s</ser:CustomFields>
+      </ser:SaleOrder>
+    SXML
 
     def initialize(hostname, username, password)
       @endpoint = BASE_URL % [hostname]
@@ -17,11 +28,41 @@ module Uniware
     end
 
     def create_sale_order(data)
-      body = {
-        "SaleOrder" => {
-
-        }
-      }
+      addresses = data.delete("Addresses")
+      items = data.delete("SaleOrderItems")
+      custom_fields = data.delete("CustomFields")
+      tmp_addresses = []
+      tmp_address_refs = []
+      addresses.keys.each_with_index do |atype,i|
+        tmp_addresses << Gyoku.xml({
+          ns_key("Address") => namespaced_hash(addresses[atype]),
+          :attributes! => {ns_key("Address") => {"id" => i+1}}
+        })
+        atype_key = ns_key(atype + "Address")
+        tmp_address_refs << Gyoku.xml({
+          atype_key => {},
+          :attributes! => {atype_key => {"ref" => i+1}}
+        })
+      end
+      order_items = items.map do |item|
+        namespaced_hash(item)
+      end
+      cfields = custom_fields.map do |f|
+        Gyoku.xml({
+          ns_key("CustomField") => {},
+          :attributes! => {
+            ns_key("CustomField") => {
+              "name" => f["name"],
+              "value" => f["value"]
+            }
+          }
+        })
+      end
+      body = SALE_ORDER_XML.gsub(/\s+/, '') % [Gyoku.xml(namespaced_hash(data)),
+                                               tmp_addresses.join,
+                                               tmp_address_refs.join,
+                                               Gyoku.xml({ns_key("SaleOrderItem") => order_items}),
+                                               cfields.join]
       perform_operation("CreateSaleOrderRequest", body)
     end
 
@@ -43,6 +84,18 @@ module Uniware
         response = client.request :ser, name do
           soap.body = body
         end
+      end
+
+      def ns_key(s)
+        'ser:' + s
+      end
+
+      def namespaced_hash(h)
+        t = {}
+        h.each do |k,v|
+          t[ns_key(k)] = v
+        end
+        return t
       end
   end
 end
---
+++ spec/uniware_spec.rb
@@ -0,0 +1,76 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
+
+describe Uniware do
+  it "must be defined" do
+    Uniware::Version.should_not be_nil
+  end
+
+  describe Uniware::Client do
+    let(:host) { 'staging.unicommerce.com' }
+    let(:username) { 'uniware_user' }
+    let(:password) { 'random_password' }
+
+    it "should create sale order" do
+      client = Uniware::Client.new(host, username, password)
+      client.create_sale_order({
+        "Code" => "Order001",
+        "DisplayOrderCode" => "Order001",
+        "DisplayOrderDateTime" => "2014-05-11T10:36:00Z",
+        "NotificationEmail" => "johndoe@gmail.com",
+        "NotificationMobile" => "+911234567890",
+        "CashOnDelivery" => true,
+        "Addresses" => {
+          "Shipping" => {
+            "Name" => "John Doe",
+            "AddressLine1" => "Shipping Address 001",
+            "City" => "Bangalore",
+            "State" => "Karnataka",
+            "Country" => "IN",
+            "Pincode" => "560001",
+            "Phone" => "+911234567890"
+          },
+          "Billing" => {
+            "Name" => "John Doe",
+            "AddressLine1" => "Billing Address 001",
+            "City" => "Bangalore",
+            "State" => "Karnataka",
+            "Country" => "IN",
+            "Pincode" => "560001",
+            "Phone" => "+911234567890"
+          }
+        },
+        "SaleOrderItems" => [
+          {"ItemSKU" => "SKU_001",
+           "ShippingMethodCode" => "STD",
+           "TotalPrice" => 10308,
+           "SellingPrice" => 10308,
+           "Discount" => 2691,
+           "Code" => 5501},
+          {"ItemSKU" => "SKU_002",
+           "ShippingMethodCode" => "STD",
+           "TotalPrice" => 5154,
+           "SellingPrice" => 5154,
+           "Discount" => 1345,
+           "Code" => "5502-1"},
+          {"ItemSKU" => "SKU_002",
+           "ShippingMethodCode" => "STD",
+           "TotalPrice" => 5154,
+           "SellingPrice" => 5154,
+           "Discount" => 1345,
+           "Code" => "5502-2"},
+          {"ItemSKU" => "SKU_003",
+           "ShippingMethodCode" => "STD",
+           "TotalPrice" => 9082,
+           "SellingPrice" => 9082,
+           "Discount" => 917,
+           "VoucherCode" => "special_offer",
+           "Code" => 5503}
+        ],
+        "CustomFields" => [
+          {"name" => "customField001", "value" => "customField001_Value"},
+          {"name" => "customField002", "value" => "customField002_Value"}
+        ]
+      })
+    end
+  end
+end
