449a456fbf82f8deac44d8b18ed6fff2a2d8576a
Amrita Koley <k.amrita@urbanladder.com>
2015-01-03 12:34:12 +0530

    OPS-37 #comment Adding code support in uniware client for switch facility api

--- lib/uniware/client.rb
+++ lib/uniware/client.rb
@@ -176,6 +176,24 @@ module Uniware
       perform_operation("CreatePurchaseOrderRequest", body, facility_endpoint("#{code}"))
     end
 
+    def switch_sale_order_item_facility_request(data, code)
+      hash_data = {"Facility" => data["Facility"],
+                   "SaleOrderCode" => data["SaleOrderCode"]}
+      sale_order_items = ""
+      if data["SaleOrderItemCodes"].present?
+        result = ""
+        data["SaleOrderItemCodes"].each do |f|
+          hash_items = {"SaleOrderItemCode" => f["SaleOrderItemCode"]}
+          items = Gyoku.xml(namespaced_hash(hash_items))
+          result += items
+        end
+      sale_order_items = "<ser:SaleOrderItemCodes>%s</ser:SaleOrderItemCodes>" % result
+      end
+      body = Gyoku.xml(namespaced_hash(hash_data))
+      body += sale_order_items
+      perform_operation("SwitchSaleOrderItemFacilityRequest", body, facility_endpoint("#{code}"))
+    end
+
     private
       def perform_operation(name, body, endpoint=nil)
         client.wsdl.endpoint = endpoint || @endpoint
